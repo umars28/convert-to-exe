b69ebe9020ccc6d9e77ec1b64aef709cf1d92809
Umar <umarsabirin369@gmail.com>
2024-06-20 09:26:16 +0800

    init auto convert

---
+++ app.py
@@ -0,0 +1,10 @@
+import webview
+
+def main():
+    # Create a webview window with the specified URL
+    webview.create_window('Garamqu', 'https://garamqu.id')
+    # Start the webview
+    webview.start()
+
+if __name__ == '__main__':
+    main()
