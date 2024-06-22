d110be41d6dab4f5ffbeb7732575c95599ec35aa
Umar <umarsabirin369@gmail.com>
2024-06-23 04:47:07 +0800

    test url staging

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ def on_loaded(window):
     print(f"Current URL: {current_url}")
 
     # Check if the URL is the specified one, then redirect
-    if current_url == "http://127.0.0.1:8000/":
-        new_url = "http://127.0.0.1:8000/cashier/login"
+    if current_url == "https://staging.garamqu.id/":
+        new_url = "https://staging.garamqu.id/cashier/login"
         print(f"Redirecting to: {new_url}")
         window.load_url(new_url)
 
 def main():
     # Create a webview window with the specified URL
-    window = webview.create_window('Kasir', 'http://127.0.0.1:8000/cashier/login', fullscreen=True)
+    window = webview.create_window('Kasir', 'https://staging.garamqu.id/cashier/login', fullscreen=True)
     
     # Add a hook to the window to handle URL changes
     window.events.loaded += on_loaded
