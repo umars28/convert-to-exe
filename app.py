cdb705d5b8b0017ec4ef735535455abcb59ab738
Umar <umarsabirin369@gmail.com>
2024-07-28 10:07:53 +0800

    change url web engine

--- app.py
+++ app.py
@@ -5,25 +5,20 @@ def on_loaded(window):
     current_url = window.get_current_url()
     print(f"Current URL: {current_url}")
 
-    # Check if the URL is the specified one, then redirect
-    if current_url == "https://staging.garamqu.id/":
-        new_url = "https://staging.garamqu.id/cashier/login"
+    if current_url == "https://inventory.personaltesting.my.id/":
+        new_url = "https://inventory.personaltesting.my.id/cashier/login"
         print(f"Redirecting to: {new_url}")
         window.load_url(new_url)
 
 def main():
-    # Get the screen size of the primary monitor
     monitor = get_monitors()[0]
     screen_width = monitor.width
     screen_height = monitor.height
 
-    # Create a webview window with the calculated size
-    window = webview.create_window('Kasir', 'https://staging.garamqu.id/cashier/login', width=screen_width, height=screen_height, x=0, y=0, resizable=True)
+    window = webview.create_window('Kasir', 'https://inventory.personaltesting.my.id/cashier/login', width=screen_width, height=screen_height, x=0, y=0, resizable=True)
     
-    # Add a hook to the window to handle URL changes
     window.events.loaded += on_loaded
     
-    # Start the webview
     webview.start()
 
 if __name__ == '__main__':
